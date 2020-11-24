be08d175df4c8adc764cb6284a8b9b0bb9c54a84
rogeliogonzalezmena@gmail.com <rogeliogonzalezmena@gmail.com>
2020-11-24 09:27:57 -0400

    hecho

---
+++ ConvertirdeDecimalaNumerosBinarios/ConvertirdeDecimalaNumerosBinarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertirdeDecimalaNumerosBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string bin = "";
+            Console.Write("Coloca el numero que quieras convertir: ");
+            n = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if ((n % 2) != 0)
+                    bin = "1" + bin;
+                else
+                    bin = "0" + bin;
+                n /= 2;
+                if (n <= 0)
+                    break;
+
+            }
+            Console.WriteLine("El numero es: " + bin);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConvertirdeDecimalaNumerosBinarios/ConvertirdeDecimalaNumerosBinarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("ConvertirdeDecimalaNumerosBinarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConvertirdeDecimalaNumerosBinarios")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("cd18df12-9cbb-486e-99cf-f5ec34396e85")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
